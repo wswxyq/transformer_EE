2bc56f7d8d88bd16083dfb7636318c420cf0e5cd
shaowei <wu000536@umn.edu>
2024-04-26 16:37:43 -0500

    update model & add example config

--- transformer_ee/model/transformerEncoder.py
+++ transformer_ee/model/transformerEncoder.py
@@ -12,29 +12,46 @@ from torch.nn import functional as F
 class Transformer_EE_MV(nn.Module):
     """
     information of slice and prongs are cancatenated together.
+    parameters:
+        d_model: int, the number of expected features in the vector input, must be divisible by nhead
+        nhead: int, the number of heads in the multiheadattention models
+        dim_feedforward: int, the dimension of the feedforward network model
+        dropout: float, the dropout value for transformer
+        num_layers: int, the number of TransformerEncoderLayer
+        linear_hidden: int, the number of hidden units in the linear layer for scalar input
+        post_linear_hidden: int, the number of hidden units in the post linear layer
     """
 
     def __init__(self, config):
         super().__init__()
-        if len(config["vector"])%len(config["target"]) != 0:
-            raise ValueError("The length of vector must be divisible by the length of target!")
         _kwargs = config["model"]["kwargs"]
+        _nhead = _kwargs.get("nhead", 4)
+        if len(config["vector"]) % _nhead != 0:
+            raise ValueError(
+                "The length of vector must be divisible by the length of target!"
+            )
         self.transformer_encoder = nn.TransformerEncoder(
             nn.TransformerEncoderLayer(
-                d_model=len(config["vector"]), # input dimension of transformer must match number of vector variables!
-                nhead=_kwargs.get("nhead", 4), # d_model must be divisible by nhead!
+                d_model=len(
+                    config["vector"]
+                ),  # input dimension of transformer must match number of vector variables!
+                nhead=_nhead,  # d_model must be divisible by nhead!
                 dim_feedforward=_kwargs.get("dim_feedforward", 2048),
                 dropout=_kwargs.get("dropout", 0.1),
                 batch_first=True,
             ),
             num_layers=_kwargs.get("num_layers", 6),
         )
-        self.linear_scalar1 = nn.Linear(3, 6)
-        self.linear_scalar2 = nn.Linear(6, 12)
+        linear_hidden = _kwargs.get("linear_hidden", 16)
+        self.linear_scalar1 = nn.Linear(len(config["scalar"]), linear_hidden)
+        self.linear_scalar2 = nn.Linear(linear_hidden, linear_hidden)
 
-        self.linear1 = nn.Linear(24, 24)
-        self.linear2 = nn.Linear(24, 24)
-        self.linear3 = nn.Linear(24, len(config["target"]))
+        post_linear_hidden = _kwargs.get("post_linear_hidden", 24)
+        self.linear1 = nn.Linear(
+            len(config["vector"]) + linear_hidden, post_linear_hidden
+        )
+        self.linear2 = nn.Linear(post_linear_hidden, post_linear_hidden)
+        self.linear3 = nn.Linear(post_linear_hidden, len(config["target"]))
 
     def forward(self, x, y, mask):
         output = self.transformer_encoder(x, src_key_padding_mask=mask)
