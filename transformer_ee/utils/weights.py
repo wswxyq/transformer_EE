ee094e5e262957b171d255f220ffe1b2f6763d40
shaowei <wu000536@umn.edu>
2024-11-19 14:34:23 -0600

    add new parameter for FlatSpectraWeights

--- transformer_ee/utils/weights.py
+++ transformer_ee/utils/weights.py
@@ -20,13 +20,21 @@ class NullWeights:
 class FlatSpectraWeights:
     """
     Initialize the weights with an array, then return the weights (as array) for a given value x.
+    The weighter will calculate the weight based on the following function using the histogram count:
+        w = 1 / (count + alpha * max (count))
+    In addition, the weighter will normalize the weights to have a mean of 1 and clip the weights to a given range.
     """
 
     def __init__(  # pylint: disable=W0622
-        self, array, bins=50, range=None, maxweight=np.inf, minweight=-np.inf
+        self, array, bins=50, range=None, maxweight=np.inf, minweight=-np.inf, alpha=0
     ) -> None:
         self.hist = np.histogram(array, bins=bins, range=range)
-        self.weights = 1 / (self.hist[0] + 1)  # Add 1 to avoid division by zero
+        _alpha = max(self.hist[0]) * alpha
+        if _alpha < 1:
+            _alpha = 1
+        self.weights = 1 / (
+            self.hist[0] + _alpha
+        )  # Add _alpha > 0 to avoid division by zero
         self.weights = self.weights / np.mean(self.weights)
         self.bins = self.hist[1]
         self.weights[self.weights > maxweight] = maxweight
